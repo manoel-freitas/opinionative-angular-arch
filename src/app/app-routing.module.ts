562486bd9497061ef88aa3cd7a1609beee5a3d1f
Manoel freitas <manoel.neto@soulisto.com>
2020-06-09 11:11:38 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,29 @@
+import { RouterModule, Routes } from '@angular/router';
+
+import { CanLoadRouteGuard } from './modules/core/guards/can-load-route.guard';
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth'
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule),
+    canLoad: [CanLoadRouteGuard]
+  },
+  {
+    path: 'transaction',
+    loadChildren: () => import('./modules/transaction/transaction.module').then(m => m.TransactionModule),
+    canLoad: [CanLoadRouteGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { SharedModule } from './modules/shared/shared.module';
+import { CanLoadRouteComponent } from './modules/core/guards/can-load-route/can-load-route.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CanLoadRouteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
